c442f5daa5f1c6838a583bc5a0e1cec01ed1d2d5
Robert <robertu.dev@gmail.com>
2022-02-27 19:36:08 -0600

    adds mobile menu

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//Mobile Menu
+
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+openMenu.addEventListener('click', show)
+closeMenu.addEventListener('click', close)
+
+function show() {
+    mainMenu.style.display = 'grid';
+    mainMenu.style.top = '0';
+}
+function close() {
+    mainMenu.style.top = '-120%';
+}
