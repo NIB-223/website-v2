d6aea7c0377067b1ef0913acab8cb2f1413cee0a
Robert <robertu.dev@gmail.com>
2022-04-03 09:18:33 -0500

    new mobile menu

--- js/main.js
+++ js/main.js
@@ -1,35 +1,4 @@
-//Mobile Menu
 
-const mainMenu = document.querySelector('.mainMenu');
-const closeMenu = document.querySelector('.closeMenu');
-const openMenu = document.querySelector('.openMenu');
-const navItems = document.querySelector('.nav-items');
-
-openMenu.addEventListener('click', show)
-closeMenu.addEventListener('click', close)
-
-function show() {
-    mainMenu.style.display = 'flex';
-    mainMenu.style.top = '0';
-}
-function close() {
-    mainMenu.style.top = '-120%';
-}
-
-//escape key closes mobile menu
-document.addEventListener('keydown', function (event) {
-    const { key } = event;
-    if (key === "Escape") {
-        mainMenu.style.top = '-120%';
-    }
-});
-
-//clicking anywhere closes mobile menu
-mainMenu.addEventListener('click', clickClose);
-
-function clickClose() {
-    mainMenu.style.top = '-120%';
-};
 
 
 //INTERSECTION OBSERVER
